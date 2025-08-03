c1877d032082830f590036970198180510a9d78b
knightchaser <agerio100@naver.com>
2025-08-03 20:32:51 +0900

    dev: read file line by line with string buffer

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 // src/main.c
 #include <stdio.h>
+#include <stdlib.h>
+
+// A reasonable maximum length for a single line of text.
+#define MAX_LINE_LENGTH 4096
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -7,7 +11,20 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("--- Preparing to show file: %s ---\n", argv[1]);
+    const char *filename = argv[1];
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char line_buffer[MAX_LINE_LENGTH] = {0};
+    while (fgets(line_buffer, sizeof(line_buffer), file) != NULL) {
+        printf("%s", line_buffer);
+    }
+
+    fclose(file);
 
     return 0;
 }

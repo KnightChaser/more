eecd86dd5c1133759f057c5bc212d701927fb84b
knightchaser <agerio100@naver.com>
2025-08-08 21:16:50 +0900

    fix: correctly match the function definition in utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,9 @@
 // src/utils.h
 #pragma once
+#include <stdbool.h>
 #include <stdio.h>
 
 int get_terminal_rows(void);
 int get_user_command(void);
 int display_page(FILE *file, int num_lines);
-int search_forward(FILE *file, const char *term, int terminal_rows);
+bool search_forward(FILE *file, const char *term, int terminal_rows);
